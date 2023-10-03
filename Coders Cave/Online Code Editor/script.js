4c29f193d5e315cf638a5b3c4780138dc5f02d72
Harshvardhan Sharma <102682161+hvs1234@users.noreply.github.com>
2023-10-03 10:37:38 +0530

    Add files via upload

---
+++ Coders Cave/Online Code Editor/script.js
@@ -0,0 +1,9 @@
+function run(){
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let outPut = document.getElementById("output");
+
+    outPut.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</st>";
+    outPut.contentWindow.eval(jsCode);
+}
